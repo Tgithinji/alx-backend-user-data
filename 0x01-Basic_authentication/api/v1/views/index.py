70283d9c10db0cfb5897fe52d0024b749fab478b
Tgithinji <t.muraguri420@gmail.com>
2024-04-16 14:16:15 +0300

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,10 @@ def unauthorized() -> str:
     """ Get /api/v1/unauthorized
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ Get /api/v1/forbidden
+    """
+    abort(403)
