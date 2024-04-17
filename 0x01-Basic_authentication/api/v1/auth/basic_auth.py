23941e7e7ed14a7f7f0a087468efd4312d4ea28b
Tgithinji <t.muraguri420@gmail.com>
2024-04-17 16:41:35 +0300

    Fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,10 +53,6 @@ class BasicAuth(Auth):
                                      ) -> TypeVar('User'):
         """returns the User instance based on his email and password.
         """
-        if user_email is None or isinstance(user_email, str):
-            return None
-        if user_pwd is None or isinstance(user_pwd, str):
-            return None
         users = User.search({'email': user_email})
         if not users or len(users) == 0:
             return None
