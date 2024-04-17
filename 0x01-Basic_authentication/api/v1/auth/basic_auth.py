4a7779cba55ee598d670095d39448f684282fd0a
Tgithinji <t.muraguri420@gmail.com>
2024-04-17 16:59:27 +0300

    Fix logic error

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,6 +53,10 @@ class BasicAuth(Auth):
                                      ) -> TypeVar('User'):
         """returns the User instance based on his email and password.
         """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
         users = User.search({'email': user_email})
         if not users or len(users) == 0:
             return None
