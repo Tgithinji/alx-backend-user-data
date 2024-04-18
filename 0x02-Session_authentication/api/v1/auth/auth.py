a6140920dd6a30ca1e296a225327a123bab6ec3d
Tgithinji <t.muraguri420@gmail.com>
2024-04-18 17:12:17 +0300

    add a new endpoint to retrieve the authenticated User object.

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" auth module """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Manage the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require auth """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        paths = []
+        path = path.rstrip('/')
+        for i in excluded_paths:
+            i = i.rstrip('/')
+            paths.append(i)
+        if path in paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization header """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current_user"""
+        print(request)
+        return None
